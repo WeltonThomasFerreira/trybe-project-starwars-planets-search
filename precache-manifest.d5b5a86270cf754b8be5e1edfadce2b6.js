ba4a0fb52c928dcf23b88bbe8df43b347c80ebbe
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-01-03 22:14:25 -0300

    Updates

---
+++ precache-manifest.d5b5a86270cf754b8be5e1edfadce2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5fa940cae3d91f73000b33446fcec0",
+    "url": "/trybe-project-starwars-planets-search/index.html"
+  },
+  {
+    "revision": "68b5a1cb38191d5d4a30",
+    "url": "/trybe-project-starwars-planets-search/static/css/main.fe527b6d.chunk.css"
+  },
+  {
+    "revision": "a21b6caadcf374f6087a",
+    "url": "/trybe-project-starwars-planets-search/static/js/2.68f463d7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/trybe-project-starwars-planets-search/static/js/2.68f463d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b5a1cb38191d5d4a30",
+    "url": "/trybe-project-starwars-planets-search/static/js/main.9724aae3.chunk.js"
+  },
+  {
+    "revision": "1efd97b390c4f7fc86aa",
+    "url": "/trybe-project-starwars-planets-search/static/js/runtime-main.1b33c893.js"
+  }
+]);
